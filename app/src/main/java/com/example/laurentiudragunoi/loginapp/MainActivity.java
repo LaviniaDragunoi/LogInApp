36be14e268fa8d6308f8b96255081cb396f6e684
LaviniaDragunoi <lavinia.dragunoi@yahoo.ro>
2018-09-27 16:07:56 +0300

    feat: Add FirebaseAuth UI functionality
    Add signIn functionality from GoogleAccount and Email.
    
    MainActivity.class, activity_main.xml

--- app/src/main/java/com/example/laurentiudragunoi/loginapp/MainActivity.java
+++ app/src/main/java/com/example/laurentiudragunoi/loginapp/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.laurentiudragunoi.loginapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,8 +22,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     public static final String ANONYMOUS = "anonymous";
-    @BindView(R.id.sign_in)
-    Button signInBttn;
+    private static final int RC_SIGN_IN = 2;
+    @BindView(R.id.browse)
+    Button browseFurtherActivity;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
 
     @Override
@@ -22,12 +36,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        signInBttn.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        browseFurtherActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, UserActivity.class);
                 startActivity(intent);
             }
         });
+     mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+         @Override
+         public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+             FirebaseUser user = firebaseAuth.getCurrentUser();
+             if(user != null){
+                 //user is signed in
+                 Toast.makeText(MainActivity.this,"You're now signed in.Welcome to my LogIn APP!",Toast.LENGTH_LONG).show();
+
+             }else {
+                 //user is signed out
+                 // Choose authentication providers
+                 List<AuthUI.IdpConfig> providers = Arrays.asList(
+                         new AuthUI.IdpConfig.EmailBuilder().build(),
+                         new AuthUI.IdpConfig.GoogleBuilder().build());
+
+// Create and launch sign-in intent
+                 startActivityForResult(
+                         AuthUI.getInstance()
+                                 .createSignInIntentBuilder()
+                                 .setIsSmartLockEnabled(false)
+                                 .setAvailableProviders(providers)
+                                 .build(),
+                         RC_SIGN_IN);
+             }
+
+         }
+     };
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
 }
